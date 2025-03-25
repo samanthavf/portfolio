fcc6264a393c86cb2bc2a7910cf9a680ddff94f4
samantha vieira <105881146+samanthavf@users.noreply.github.com>
2025-03-24 23:57:22 -0300

    instructions commit

--- src/app/portifolio/portifolio.component.ts
+++ src/app/portifolio/portifolio.component.ts
@@ -1,10 +1,14 @@
-import { isPlatformBrowser } from "@angular/common";
+import { CommonModule, isPlatformBrowser } from "@angular/common";
 import { AfterViewInit, Component, Inject, PLATFORM_ID } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-portifolio',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl:'./portifolio.component.html',
   styleUrl: './portifolio.component.css'
 })
@@ -15,21 +19,41 @@ export class PortifolioComponent implements AfterViewInit {
 
     constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+    modalAberto = false;
+    projetoAtual = { titulo: '', descricao: '', imagem: '' };
+
+    projetos = [
+        { titulo: 'Projeto 1', descricao: 'Descrição do projeto 1', imagem: 'projeto1.png' },
+        { titulo: 'Projeto 2', descricao: 'Descrição do projeto 2', imagem: 'projeto2.png' }
+      ];
+
+abrirModal(projetoIndex: number) {
+  this.projetoAtual = this.projetos[projetoIndex];
+  this.modalAberto = true;
+}
+
+fecharModal() {
+  this.modalAberto = false;
+}
+
   private walls =[
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     [1,1,1,0,1,1,1,0,1,1,1,1,0,1,1,1],
     [1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1],
-    [1,0,1,1,1,1,0,1,1,1,1,1,0,1,0,1],
+    [1,0,1,0,0,0,0,1,0,0,2,1,0,1,0,1],
+    [1,0,1,1,1,1,0,1,1,1,1,1,2,1,0,1],
     [1,0,0,0,0,1,0,1,0,0,0,1,1,1,0,1],
+    [1,0,0,0,2,1,0,1,2,0,0,1,0,0,0,1],
     [1,0,1,1,1,1,0,1,1,1,0,1,0,0,0,1],
-    [1,0,1,1,0,0,0,0,0,0,0,0,0,1,0,1],
-    [1,0,0,1,0,1,0,1,1,1,0,1,1,1,0,1],
-    [1,1,0,1,1,1,0,1,0,0,0,1,0,1,0,1],
-    [1,1,0,0,0,0,0,1,0,1,0,1,0,0,0,1],
+    [1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1],
+    [1,0,1,2,0,1,0,1,1,1,0,1,1,1,0,1],
+    [1,0,1,1,1,1,0,1,0,1,0,1,2,1,0,1],
+    [1,0,0,0,0,0,0,1,2,0,0,1,0,0,0,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
   ];
 
   private character = {img: null as HTMLImageElement | null, x:0,y:0,size:50};
+  private card = {img: null as HTMLImageElement | null}
 
   ngAfterViewInit(){
     if (isPlatformBrowser(this.platformId)) {
@@ -47,6 +71,13 @@ export class PortifolioComponent implements AfterViewInit {
       console.log('Imagem carregada');
       this.draw();
       };
+
+      this.card.img = new Image();
+      this.card.img.src = 'cenoura.png'
+      this.card.img.onload = () => {
+        console.log('Card Imagem carregada!');
+        this.draw();
+      }
 
       this.canvas.addEventListener('click', (event) => this.handleMouseMove(event));
     }
@@ -69,13 +100,20 @@ export class PortifolioComponent implements AfterViewInit {
     const cellX = Math.floor(clickX / cellSize);
     const cellY = Math.floor(clickY / cellSize);
   
-    if (this.walls[cellY] && this.walls[cellY][cellX] === 0) {
+    if (this.walls[cellY] && this.walls[cellY][cellX] === 0 ) {
       this.character.x = cellX;
       this.character.y = cellY;
       this.draw();
-    }
+  }else if (this.walls[cellY] && this.walls[cellY][cellX] === 2) {
+    console.log('Visualizou um projeto!');
+    this.walls[cellY][cellX] = 0
 
+    const projetoIndex = Math.floor(Math.random() * this.projetos.length);
+    this.abrirModal(projetoIndex);
+
+    this.draw()
   }
+}
 
   drawCharacter() {
     if (this.character.img && this.character.img.complete) {
@@ -95,11 +133,34 @@ export class PortifolioComponent implements AfterViewInit {
 
     for(let row=0; row < this.walls.length; row++){
       for(let col=0; col < this.walls[row].length; col++){
-        if(this.walls[row][col] === 1){
+        if(this.walls[row][col] === 1 ){
           this.ctx.fillStyle = 'white';
           this.ctx.fillRect(col*cellSize,row *cellSize,cellSize,cellSize);
+        }else if (this.walls[row][col] === 2 && this.card.img) {
+          this.ctx.drawImage(
+            this.card.img,
+            col * cellSize + (cellSize / 4),  // Ajuste para centralizar
+            row * cellSize + (cellSize / 4),
+            cellSize / 2, // Reduz o tamanho do card para melhor visualização
+            cellSize / 2
+          );
         }
       }
     }
   }
+
+checkCollision(x: number, y: number){
+  const row = Math.floor(y/50);
+  const col = Math.floor(x/50);
+
+  if (this.walls[row][col] === 2) {
+    console.log('Visualizou projeto!')
+    this.walls[row][col] = 0;
+
+    const projetoIndex = Math.floor(Math.random() * this.projetos.length);
+    this.abrirModal(projetoIndex);
+
+    this.draw();
+  }
+}
 }
